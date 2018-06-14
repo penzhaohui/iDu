c0bd26df4ebc68219623d013b79aeaf45d9eaf15
pen_zhaohui@126.com <xiangning520>
2018-06-14 21:38:28 +0800

    来自于 https://github.com/cytex124/odoo-drag-and-drop

--- odoo-addons/openacademy/controllers/controllers.py
+++ odoo-addons/openacademy/controllers/controllers.py
@@ -36,3 +36,28 @@ class Openacademy(http.Controller):
     # def teacher1(self, **kw):
     #     # env['openacademy.teacher'].getAllTeacher1()
     #     return ''
+
+    @http.route('/w/download', type='http', auth='public', csrf=False)
+    def w_download_attachment(self, **kwargs):
+        attachment_id = kwargs.get('attachment_id')     #取得前端传回来的id
+        # 根据id取得数据库中对应的附件，其中datas就是我们的附件数据
+        attachment = request.env['ir.attachment'].sudo().search_read(
+            [('id', '=', int(attachment_id))],
+            ["name", "datas", "res_model", "res_id", "type", "url"]
+        )
+        if attachment:
+            attachment = attachment[0]
+        else:
+            return redirect('/w/download')
+
+        res_id = attachment['res_id']
+        if attachment["type"] == "url":
+            if attachment["url"]:
+                return redirect(attachment["url"])
+            else:
+                return request.not_found()
+        elif attachment["datas"]:
+            data = StringIO(base64.standard_b64decode(attachment["datas"]))
+            return http.send_file(data, filename=attachment['name'], as_attachment=True)
+        else:
+            return request.not_found()
