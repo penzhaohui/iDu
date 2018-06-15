59c15c68c48dc1aaf394915bd34c2946b73b892a
pen_zhaohui@126.com <xiangning520>
2018-06-15 23:04:05 +0800

    再新增一些Widget的用例学习 https://www.kancloud.cn/hx78/odoo_10/416226 https://odooforbeginnersblog.wordpress.com/2017/03/09/widgets-in-odoo/

--- odoo-addons/openacademy/models/Widget.py
+++ odoo-addons/openacademy/models/Widget.py
@@ -14,6 +14,7 @@ class Widget(models.Model):
     state = fields.Selection(string='What do you want ?', selection='_get_selection')
     logo = fields.Binary(string="Upload Logo")
     email = fields.Char(string='Widget Email', size=30)
+    duration = fields.Float('Duration', default=1.5)
     taxes_id = fields.Many2many('openacademy.course', string='Course')
     date_planned = fields.Datetime(string='Scheduled Date', store=True, index=True)
     progress = fields.Float('Widget Progress', digits=(0, 2))
@@ -29,15 +30,25 @@ class Widget(models.Model):
     description = fields.Char('Field Char', size=50)
     description_pad = fields.Char('Pad URL', pad_content_field='description')
     memo = fields.Html('Note Content')
+    member_ids = fields.One2many('res.users', 'partner_id', string='Team Members')
 
     integer = fields.Integer('Field Integer')
     description = fields.Char('Field Char', size=5)
     text = fields.Text('Field Text')
     date = fields.Date('Field Name')
     dateTime = fields.Datetime('Field Time')
+    body_html = fields.Html('Body HTML', translate=True, sanitize=False)
+    subtotal = fields.Boolean('Add subtotal', default=True)
+    reference = fields.Char(string='Reference', compute='_compute_reference', readonly=True, store=False)
+
+    def _compute_reference(self):
+        for res in self:
+            res.reference = "%s,%s" % ("Test", 100)
 
     def _get_selection(self):
         return ([
             ('draft', 'Draft'),
             ('sent', 'Sent'),
             ('cancel', 'Cancelled'),])
+
+    total_amount = fields.Float(string="Total Amount", readonly=True, digits=(0, 2))
